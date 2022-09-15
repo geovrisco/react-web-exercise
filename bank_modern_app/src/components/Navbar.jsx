68feaf7cdfa98195dc69a0f08ea9dbf62537e6f4
GABRIEL GEOVRISCO <GABRIEL.GEOVRISCO@btpn.com>
2022-09-15 16:01:52 +0700

    first commit

---
+++ bank_modern_app/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      tesad
+    </nav>
+  )
+}
+
+export default Navbar
